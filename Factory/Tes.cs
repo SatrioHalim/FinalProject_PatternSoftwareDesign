a43ccf56c55c2a6127a54b930ec60fb0f57ad7a2
SatrioHalim <satriohalim21@gmail.com>
2025-05-19 13:05:11 +0700

    Tes

---
+++ Factory/Tes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject_PSD_EmpatSekawan.Factory
+{
+    public class Tes
+    {
+    }
+}
